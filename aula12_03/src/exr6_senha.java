46805576f16deefbb70bc5e49dc87b6cdb4dd148
TriadeMat <matheusnascimento.26.mt@gmail.com>
2026-03-19 15:25:50 -0300

    1

--- aula12_03/src/exr6_senha.java
+++ aula12_03/src/exr6_senha.java
@@ -3,9 +3,9 @@ public class exr6_senha {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 System.out.println("informe sua senha: ");
-var senha = scan.next();
+int senha = scan.nextInt();
 
-if (senha == ){
+if (senha == 1312 ){
     System.out.println("Senha correta!!");    
 }else{
     System.out.println("Senha Incorreta!!");
